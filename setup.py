9990a8048b59c05e8e3d40c5faafd110e3f158eb
presidentofyes12 <nkenna@iheakaram.com>
2020-12-31 12:31:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ import os
 import sys
 import requests
 pythonloc = os.path.dirname(sys.executable)
-cont = requests.get('https://raw.githubusercontent.com/presy12/testrepo/main/doreq.py').content
-print("Setting up 'doreq'...")
-with open('req.py', 'w') as req:
-  req.write(cont.decode("utf-8"))
-print("Adding to " + pythonloc + "...")
-os.rename('req.py', pythonloc + '\doreq.py')
+if os.path.isfile(pythonloc + "/doreq.py"):
+  print("Library 'doreq' already installed")
+else:
+  cont = requests.get('https://raw.githubusercontent.com/presy12/testrepo/main/doreq.py').content
+  print("Setting up 'doreq'...")
+  with open('req.py', 'w') as req:
+    req.write(cont.decode("utf-8"))
+  print("Adding to " + pythonloc + "...")
+  os.rename('req.py', pythonloc + '\doreq.py')
